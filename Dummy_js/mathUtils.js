04a4acccd8032f6fd8ca5a9f95289347d61fd7de
Jake Batucan <jake.batucan@awsys-i.com>
2025-11-25 14:54:46 +0800

    Introduce bug in multiply funciton for PR testing

--- Dummy_js/mathUtils.js
+++ Dummy_js/mathUtils.js
@@ -8,7 +8,7 @@ function add(a, b) {
   
   // This will be intentionally "wrong" in next PR
   function multiply(a, b) {
-    return a + b; // Bug! should be a * b
+    return a - b; // Bug! should be a * b
   }
   
   module.exports = { add, subtract, multiply };
